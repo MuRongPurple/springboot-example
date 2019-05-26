cfd500ed619078f61abac9d994a4a26b66cd3ee4
Denny <15008243853@126.com>
2019-05-26 11:35:16 +0800

    #comments 拦截器例子

--- src/main/java/com/murongpurple/org/SpringbootExampleApplication.java
+++ src/main/java/com/murongpurple/org/SpringbootExampleApplication.java
@@ -3,6 +3,9 @@ package com.murongpurple.org;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Denny
+ */
 @SpringBootApplication
 public class SpringbootExampleApplication {
 
--- src/main/java/com/murongpurple/org/controller/TestAopController.java
+++ src/main/java/com/murongpurple/org/controller/TestAopController.java
@@ -1,9 +1,11 @@
 package com.murongpurple.org.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestAopController {
 
@@ -11,6 +13,7 @@ public class TestAopController {
     @GetMapping("/index2")
     public Object index2(){
         System.out.println("方法2执行");
+        log.info("hello murongpurple");
         return "hello murongpurple";
     }
 }
